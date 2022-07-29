e9017c9ba260b29e64fd31385e7b767436be20f7
wilkyrlx <john_wilkinson@brown.edu>
2022-07-29 01:31:43 -0400

    prefer_canvas to improve render

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import folium
 import os
 
-mapObj = folium.Map()
+mapObj = folium.Map(prefer_canvas=True)
 
 folium.GeoJson("geojson-data\\Squam_Test.geojson", name="Squam Lake").add_to(mapObj)
 folium.GeoJson("geojson-data\\China_Lakes.geojson", name="China").add_to(mapObj)
