b5f0fc62ce9f1db02b9e1d4d6f149201c4bffddd
wilkyrlx <john_wilkinson@brown.edu>
2022-07-28 23:42:25 -0400

    csv working 2

--- csv_converter.py
+++ csv_converter.py
@@ -1,7 +1,9 @@
 import csv
 
-input = open('World_Lakes.csv', 'r')
-output = open('Shortened.csv', 'w', newline='')
+filepath = "input-files\\"
+
+input = open(filepath + 'World_Lakes.csv', 'r')
+output = open(filepath + 'North_America_Lakes.csv', 'w', newline='')
 writer = csv.writer(output)
 for row in csv.reader(input):
     if row[3] == "United States of America" or row[3] == "Canada":
