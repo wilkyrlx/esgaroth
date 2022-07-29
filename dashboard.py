bd1c048b0560103e943550ec3d6cc638db81a231
wilkyrlx <john_wilkinson@brown.edu>
2022-07-29 01:33:53 -0400

    test

--- dashboard.py
+++ dashboard.py
@@ -7,4 +7,3 @@ folium.GeoJson("geojson-data\\Squam_Test.geojson", name="Squam Lake").add_to(map
 folium.GeoJson("geojson-data\\China_Lakes.geojson", name="China").add_to(mapObj)
 
 mapObj.save('html-output\\output.html')
-
